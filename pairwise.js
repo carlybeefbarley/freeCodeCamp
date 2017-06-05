21028c9ce3514f514ac1c8ec61c71ff90f20a99e
ahituna6 <carlydanae1@gmail.com>
2017-06-05 13:43:07 -0700

    Pairwise free code camp advanced algorithm scripting
    Given an array arr, find element pairs whose sum equal the second
    argument arg and return the sum of their indices.
    
    If multiple pairs are possible that have the same numeric elements but
    different indices, return the smallest sum of indices. Once an element
    has been used, it cannot be reused to pair with another.
    
    For example pairwise([7, 9, 11, 13, 15], 20) returns 6. The pairs that
    sum to 20 are [7, 13] and [9, 11]. We can then write out the array with
    their indices and values.

---
+++ pairwise.js
@@ -0,0 +1,22 @@
+
+function pairwise(arr, arg) {
+//give the sum a placeholder of 0 to begin with
+  var sum = 0;
+  //slice the array so that numbers can be worked with individually
+  var splitArr = arr.slice();
+  //begin by looping through the first element of the array 
+  for (var i = 0; i < splitArr.length; i++){
+    //then loop starting with the second element so that the numbers can be checked against one another
+    for (var j = i + 1; j < splitArr.length; j++){
+      if (splitArr[i] + splitArr[j] === arg){
+        sum += i + j;
+        //if there are multiple of the same number, make the second NaN
+        splitArr[i] = splitArr[j] = NaN;
+      }
+    }
+    
+  }
+  return sum;
+}
+
+pairwise([1,4,2,3,0,5], 7);
