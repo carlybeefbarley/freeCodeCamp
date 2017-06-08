b4f5527a69df184fce6b14c4316cec5df27f5abb
ahituna6 <carlydanae1@gmail.com>
2017-06-08 13:27:01 -0700

    Finished Calculator
    final product for free code camp JavaScript calculator project

--- calculator/js/javascript.js
+++ calculator/js/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  //store input from user in array
+
   var inputs = [""];
-  //string to store current input
+
   var totalString;
   //operators array w/ and w/o the "."
   var operators1 = ["*", "/", "-", "+"];
@@ -9,38 +9,39 @@ $(document).ready(function(){
   var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
   function getValue(input){
-    if(operators2.includes(inputs[inputs.length-1] === true && input = ".")){
-      console.log("Duplicate '.'");
-    }else if(inputs.length === 1 && operators1.includes(input)===false){
-             inputs.push(input);
-             } else if(operators1.includes(inputs[inputs.length-1]) === false){
-               inputs.push(input);
-             }
-    else if (nums.includes(Number(input))){
-             inputs.push(input);
-             }
+    if(operators2.includes(inputs[inputs.length -1] === true && input === '.')){
+      console.log('Duplicate "."');
+    } else if(inputs.length === 1 && operators1.includes(input) === false){
+              inputs.push(input);
+     } else if (operators1.includes(inputs[inputs.length - 1]) === false){
+              inputs.push(input);
+      } else if (nums.includes(Number(input))){
+              inputs.push(input);
+      } 
     update();
   }
   
   function update(){
-    totalString = inputs.join("");
-    $("#steps").html(totalString);
+   totalString = inputs.join(''); 
+    $('#steps').html(totalString);
+    
   }
   
   function getTotal(){
-    totalString = inputs.join("");
-    $("#steps").html(eval(totalString));
+    totalString = inputs.join('');
+    $('#steps').html(eval(totalString)); 
   }
-  $("#a").on("click", function(){
-    if(this.id === "deleteAll"){
-      inputs=[""];
+  $('a').on('click', function(){
+    if(this.id === 'deleteAll'){
+      inputs = [''];
       update();
-    } else if (this.id === "backOne"){
+    } else if (this.id === 'backOne'){
       inputs.pop();
-    } else if (this.id === "total"){
+      update();
+    } else if (this.id === 'total'){
       getTotal();
-    } else{
-      if(inputs[inputs.length-1].indexOf("+", "-", "/", "*") === -1){
+    }else{
+      if(inputs[inputs.length - 1].indexOf('+', '-', '/','*')=== -1){
          getValue(this.id);
          }else{
          getValue(this.id);
